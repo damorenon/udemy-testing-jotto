e9e608063ee248767b04a6d59ab532377bbe8c98
damorenon <damorenon@gmail.com>
2021-05-01 19:50:36 -0500

    Add proptypes for Congrats component

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,21 @@
+import checkPropTypes from "check-prop-types";
+
+/**
+ * Return node(s) with the given data-test attribute
+ * @param {ShallowWrapper} shallowWrapper
+ * @param {string } value
+ * @returns {ShallowWrapper}
+ */
 export const findByTestAttr = (shallowWrapper, value) => {
   return shallowWrapper.find(`[data-test="${value}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
