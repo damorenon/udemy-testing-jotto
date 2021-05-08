403312ecdc16f8be91657c2867ee8daf255fc1f4
damorenon <damorenon@gmail.com>
2021-05-08 10:50:43 -0500

    Add helper function for letter matched between guess and secret word

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const Input = () => {
-  return <div data-test="component-input"></div>;
+const Input = ({ success, secretWord }) => {
+  const [currentGuess, setCurrentGuess] = useState("");
+
+  if (success) {
+    return <div data-test="component-input" />;
+  }
+
+  return (
+    <div data-test="component-input">
+      <form action="" className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="Enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(evt) => {
+            evt.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,15 +1,94 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+
+const mockSetCurrentGuess = jest.fn();
+
+// Hack for hooks
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: (initialState) => [initialState, mockSetCurrentGuess],
+}));
+
+const defaultProps = { secretWord: "", success: false };
 
 const setup = (props = {}) => {
-  const setupProps = { ...props };
+  const setupProps = { ...defaultProps, ...props };
   return shallow(<Input {...setupProps} />);
 };
 
-test("renders without error", () => {
-  const wrapper = setup();
-  const component = findByTestAttr(wrapper, "component-input");
-  expect(component.length).toBe(1);
+describe("render", () => {
+  describe("Success is true", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = setup({ success: true });
+    });
+
+    test("renders without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+
+    test("input box does not show", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.exists()).toBe(false);
+    });
+
+    test("Submit button does not show", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(false);
+    });
+  });
+
+  describe("Success is false", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = setup({ success: false });
+    });
+
+    test("renders without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+
+    test("input box shows", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.exists()).toBe(true);
+    });
+
+    test("Submit button shows", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.exists()).toBe(true);
+    });
+  });
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { secretWord: "any" };
+  checkProps(Input, expectedProps);
+});
+
+describe("State controlled input field", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    wrapper = setup();
+  });
+
+  test("state updates with value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
 });
