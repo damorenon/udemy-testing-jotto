d1af97cc1d2d776a63305a09411ba971dde7b216
damorenon <damorenon@gmail.com>
2021-05-17 11:47:35 -0500

    add actions mock

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import { useEffect } from "react";
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 import Input from "./Input";
+import { getSecretWord } from "./actions";
 
 function App() {
   // TODO: get props from shared state
   const success = false;
   const secretWord = "party";
-  const gussedWords = [];
+  const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={success} />
       <Input success={success} secretWord={secretWord} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,41 @@
-import { shallow, ShallowWrapper } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "../test/testUtils";
 import App from "./App";
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock("./actions");
+import { getSecretWord as mockGetSecretWord } from "./actions";
 
 /**
  * Setup function for App component
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test("renders without error", () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent).toHaveLength(1);
+});
+
+describe("get secret word", () => {
+  beforeEach(() => {
+    // clear the mock calls from previous tests
+    mockGetSecretWord.mockClear();
+  });
+
+  test("getSecretWord on app mount", () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test("getSecretWord does not run on app update", () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    //using setProps becase wrapper.update() doesn't trigger useEffect
+    wrapper.setProps();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
